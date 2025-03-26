75769fd3ead3630566fc93e8f9e68eec29779c17
Giffareno237 <giffareno05@outlook.fr>
2025-03-26 13:23:53 +0100

    𝙆𝙀𝙍𝙈 𝙈𝘿

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ _  ______   _____ _____ _____ _   _
 |_|\_\____|   |_| |_____\____|_| |_|
 
 ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
-CONTACT ME HERE +237656520674
+CONTACT ME HERE +237656520674/
 YT: KermHackTools
 Github: Kgtech-cmr
 */
